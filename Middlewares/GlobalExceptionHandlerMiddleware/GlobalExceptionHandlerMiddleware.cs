0c3b89171c836f44c59c98d36cfaa548a608aa20
PedroHenriqDev <devpedrohenri@gmail.com>
2025-09-26 21:57:53 -0300

    Add global exception handler middleware.

---
+++ Middlewares/GlobalExceptionHandlerMiddleware/GlobalExceptionHandlerMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace WebBoost.Filters
+{
+    public class GlobalExceptionHandlerMiddleware : IMiddleware
+    {
+        private readonly ILogger<GlobalExceptionHandlerMiddleware> _logger;
+
+        public GlobalExceptionHandlerMiddleware(ILogger<GlobalExceptionHandlerMiddleware> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                _logger.LogCritical($"Exception: {ex.InnerException} \n Message: {ex.Message} \n Stack Trace: {ex.StackTrace} \n Source: {ex.Source}");
+                await context.Response.WriteAsJsonAsync(new { Message = "Internal server error" });
+            }
+        }
+    }
+}
---
+++ Middlewares/MiddlewareExtension.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using WebBoost.Filters;
+
+namespace WebBoost.Middlewares
+{
+    public static class MiddlewareExtension
+    {
+        public static IApplicationBuilder UseGlobalExceptionHandler(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<GlobalExceptionHandlerMiddleware>();
+            return app;
+        }
+
+        public static IServiceCollection AddGlobalExceptionHandler(this IServiceCollection services)
+        {
+            services.AddSingleton<GlobalExceptionHandlerMiddleware>();
+            return services;
+        }
+    }
+}
